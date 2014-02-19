7a2e08a293ae9fbd43a8633b7193b744f89c7531
Mindy Preston <meetup@yomimono.org>
2014-02-19 18:54:45 -0500

    Redundant code removed; main logic refactored

--- tictactoe.hs
+++ tictactoe.hs
@@ -9,8 +9,7 @@ getHumanPlay :: IO (Integer, Integer)
 getHumanPlay = do  
         putStrLn $ "Please choose your move by inputting a pair, e.g. (1, 1)."
         moveChosen <- getLine
-        return (r moveChosen)
-                where r = read :: String -> (Integer, Integer)
+        return (read moveChosen)
 
 humanPlay :: Board -> IO (Integer, Integer)
 humanPlay board = do
@@ -23,19 +22,17 @@ mainLoop :: (Board, Player, Integer) -> IO (Board, Player, Integer)
 mainLoop (board, player, size) = do
 	putStrLn ""
 	putStrLn $ showBoardState size board
-	if gameOver board size
-		then do
-			putStrLn "GAME OVER"
-			if (winner board size /= Just Unclaimed)
-				then do
-					putStrLn ("The winner is player "  ++ (show $ Maybe.fromJust (winner board size)) ++ ".")
-				else do
-					putStrLn "The game was a draw."
+	case winner board size of
+		Just Unclaimed -> do
+			putStrLn "The game was a draw."
 			return (board, player, size)
-		else 
+		Just x -> do
+			putStrLn  ("The winner is player "  ++ ((show x) ++ "."))
+			return (board, player, size)
+		Nothing ->
 			if player == Player1 then do
 				nextHumanPlay <- (humanPlay board) 
-				mainLoop ((advancePlay nextHumanPlay player board), nextPlayer player , size)
+				mainLoop ((advancePlay nextHumanPlay player board), nextPlayer player, size)
 			else 
 				mainLoop (nextBoard, nextPlayer(player), size)
 				where 	nextBoard = mid (nextGameState(player, board, size))
