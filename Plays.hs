3343a2c304b5ac3c1935a2f451a7890d15d817ee
Mindy Preston <meetup@yomimono.org>
2014-04-11 15:18:14 -0400

    Pull leaf score calculations into their own function.

--- Plays.hs
+++ Plays.hs
@@ -38,19 +38,31 @@ advancePlay (x, y) player (Board board size) =
 
 intermediateScore :: Integer -> Player -> Board -> Integer
 intermediateScore morePlies player board
-        | gameOver board = Maybe.fromJust (score board)
-	| morePlies <= 0 = 
-		if List.null (winningPlays player board) then 0
-		else if player == Player1 then 1
-		else -1
-        | otherwise = --make a list of boards with the next play in them, map the list to intermediate scores, return the maximum or minimum as approproate
-                playerEval (nextPlayer player) (List.map (intermediateScore (morePlies - 1) (nextPlayer player)) nextPlays)
-                where   nextPlays = [ advancePlay p (nextPlayer player) board | p <- nonDuplicatePlays board ]
+  | gameOver board = Maybe.fromJust (score board)
+	| morePlies <= 0 = leafScore player board
+  | otherwise = --make a list of boards with the next play in them, map the list to intermediate scores, return the maximum or minimum as appropriate
+			if List.null possibilities then leafScore player board 
+			else playerEval adversary (List.map (intermediateScore (morePlies - 1) adversary) possibilities)
+      where     
+				adversary = nextPlayer player
+				possibilities = nextPlays adversary board
+
+--need a memoizer of type Memo.Integral, Memo.Player, Memo.Board
+leafScore :: Player -> Board -> Integer
+leafScore player board 
+		| List.null (winningPlays player board) = 0
+		| player == Player1 = 1
+		| otherwise = -1
+
+nextPlays :: Player -> Board -> [ Board ]
+nextPlays nextPlayer board =
+				[ advancePlay p nextPlayer board | p <- nonDuplicatePlays board ]
 
 bestMove :: Player -> Board -> (Integer, Integer)
 bestMove player board =
         snd (List.foldl' fitness acc possibleBoards) 
                 where   possibleBoards = 
+												-- TODO: memoize this computation.
                                 [ (intermediateScore 5 player (advancePlay p player board), p) | p <- nonDuplicatePlays board ] 
                         fitness = pairEval (pairPlayerEval player)
                         acc = (playerBound player, undefined)
