422f401d8db207ba02bd4847343e3736d0d5f532
Mindy Preston <meetup@yomimono.org>
2014-04-14 17:13:22 -0400

    Make a data type for board state scores

--- Plays.hs
+++ Plays.hs
@@ -7,7 +7,8 @@ import Data.List as List
 import Data.Map as Map
 import Data.Maybe as Maybe
 
-data BoardState = BoardState Player Board deriving Ord
+data BoardState = BoardState Player Board deriving (Ord, Show)
+data Score = Score Integer deriving (Ord, Eq, Show)
 
 instance Eq BoardState where
   (BoardState player1 board1) == (BoardState player2 board2) = 
@@ -21,19 +22,17 @@ playerEval :: Ord a => Player -> [a] -> a
 playerEval Player1 = List.maximum
 playerEval Player2 = List.minimum
 
-playerBound :: Player -> Integer
-playerBound Player1 = -100000
-playerBound Player2 =  100000
+playerBound :: Player -> Score
+playerBound Player1 = Score (-100000)
+playerBound Player2 = Score 100000
 
 pairPlayerEval :: Player -> Ordering
 pairPlayerEval Player1 = GT
 pairPlayerEval Player2 = LT
 
-pairEval :: Ord a => Ordering -> (a, b) -> (a, b) -> (a, b)
-pairEval ordering pair other =
-        if compare (fst pair) (fst other) == ordering then
-                pair
-        else other
+pairEval :: Ordering -> (Score, b) -> (Score, b) -> (Score, b)
+pairEval ordering (pair,x) (other,y) = 
+        if compare pair other == ordering then (pair, x) else (other, y)
 
 validPlay :: (Integer, Integer) -> Board -> Bool
 validPlay point (Board board size) =
@@ -43,28 +42,28 @@ advancePlay :: (Integer, Integer) -> Player -> Board -> Board
 advancePlay point player (Board board size) =
         Board (board Array.// [ (point, player) ] ) size
 
-intermediateScore :: Map BoardState Integer -> Integer -> Player -> Board -> (Map BoardState Integer, Integer)
+intermediateScore :: Map BoardState Score -> Integer -> Player -> Board -> (Score, Map BoardState Score)
 intermediateScore memoTable plies player board =
 				--map of the tuple (player, board) to integer - scores are dependent on who's to play next
 				case Map.lookup (BoardState player board) memoTable of 
-								Just p -> (memoTable, p)
+								Just p -> (p, memoTable)
 								Nothing -> intermediateScore' memoTable plies player board
 
-whichScore :: Map BoardState Integer -> Integer -> Player -> Board -> (Map BoardState Integer, Integer)
+whichScore :: Map BoardState Score -> Integer -> Player -> Board -> (Score, Map BoardState Score)
 whichScore memoTable plies player board =
   if gameOver board || List.null possibilities || plies <= 0 then 
 				case score board of
-								Just x -> (memoTable, x)
-								Nothing -> (memoTable, 0)
-	else playerEval adversary (List.map (intermediateScore memoTable (plies - 1) adversary) possibilities) 
+								Just x -> (Score x, memoTable)
+								Nothing -> (Score 0, memoTable)
+	else playerEval adversary (List.map (intermediateScore memoTable (plies - 1) adversary) possibilities)
 				where 
 				adversary = nextPlayer player
 				possibilities = nextPlays adversary board
 
-intermediateScore' :: Map BoardState Integer -> Integer -> Player -> Board -> (Map BoardState Integer, Integer)
+intermediateScore' :: Map BoardState Score -> Integer -> Player -> Board -> (Score, Map BoardState Score)
 intermediateScore' memoTable plies player board =
-				(Map.insert (BoardState player board) score newMap, score)
-				where (newMap, score) = whichScore memoTable plies player board
+				(score, Map.insert (BoardState player board) score newMap)
+				where (score, newMap) = whichScore memoTable plies player board
 
 nextPlays :: Player -> Board -> [ Board ]
 nextPlays nextPlayer board =
@@ -72,13 +71,17 @@ nextPlays nextPlayer board =
 
 bestMove :: Player -> Board -> (Integer, Integer)
 bestMove player board =
-				snd (List.foldl' fitness acc possibleBoards) 
-                where   
-								openSpots = nonDuplicatePlays board
-								depthLimit = if List.length openSpots > 3 then 2 else 5
-								possibleBoards = [ (snd $ intermediateScore (Map.fromList []) depthLimit player (advancePlay p player board), p) | p <- openSpots ] 
-								fitness = pairEval (pairPlayerEval player)
-								acc = (playerBound player, undefined)
+				case (winningPlays player board, winningPlays (nextPlayer player) board) of
+								(x : [], _) -> x
+								([], x : []) -> x
+								_ ->
+												snd (List.foldl' fitness acc possibleBoards) 
+												where   
+												openSpots = nonDuplicatePlays board
+												depthLimit = 1000 --if List.length openSpots > 5 then 1 else 5
+												possibleBoards = [ (fst $ intermediateScore (Map.fromList []) depthLimit player (advancePlay p player board), p) | p <- openSpots ] 
+												fitness = pairEval (pairPlayerEval player)
+												acc = (playerBound player, undefined)
 
 nextGameState :: (Player, Board) -> (Player, Board)
 nextGameState (player, board)
